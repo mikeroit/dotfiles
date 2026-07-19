16f79d878c2b175833d08c3563afca8544f5b20b
mikeroit <mikeroit@gmail.com>
2026-07-19 01:54:39 -0600

    Import Ubuntu development configuration

---
+++ scripts/link-dotfiles.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+REPO_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")/.." && pwd)"
+STOW_DIR="$REPO_DIR/stow"
+TARGET_DIR="${HOME:?HOME is not set}"
+
+if ! command -v stow >/dev/null 2>&1; then
+    echo "GNU Stow is not installed." >&2
+    exit 1
+fi
+
+cd "$STOW_DIR"
+
+for package in bash git nvim; do
+    if [[ -d "$package" ]]; then
+        echo "Linking $package"
+        stow --restow --target="$TARGET_DIR" "$package"
+    fi
+done
---
+++ stow/nvim/.config/nvim/init.lua
@@ -0,0 +1,159 @@
+-- Basic settings
+vim.g.mapleader = " "
+
+vim.keymap.set({ "n", "v", "o" }, ";", ":", { noremap = true })
+
+-- nav between files
+vim.keymap.set("n", "<leader>p", "<C-^>", { desc = "Previous file" })
+vim.keymap.set("n", "<leader>q", "<cmd>bprevious | bdelete #<CR>", { desc = "Close buffer" })
+vim.keymap.set("n", "<leader>bn", "<cmd>bnext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<leader>bp", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 250
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  -- Rust / LSP
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      vim.lsp.config("rust_analyzer", {
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true,
+            },
+            check = {
+              command = "clippy",
+            },
+            inlayHints = {
+              typeHints = { enable = true },
+              parameterHints = { enable = true },
+            },
+          },
+        },
+      })
+
+      vim.lsp.enable("rust_analyzer")
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(event)
+          local opts = { buffer = event.buf }
+
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "<leader>f", function()
+            vim.lsp.buf.format({ async = true })
+          end, opts)
+        end,
+      })
+    end,
+  },
+
+  -- Completion
+  {
+    "saghen/blink.cmp",
+    version = "*",
+    opts = {
+        keymap = {
+            preset = "default",
+
+            ["<Tab>"] = { "accept", "fallback" },
+            ["<CR>"] = { "accept", "fallback" },
+
+            ["<C-j>"] = { "select_next" },
+            ["<C-k>"] = { "select_prev" },
+
+            ["<C-Space>"] = { "show" },
+        },
+    }
+  },
+      -- Automatically insert matching parentheses, brackets, and quotes
+  {
+    "nvim-mini/mini.pairs",
+    version = false,
+    config = function()
+      require("mini.pairs").setup()
+    end,
+  },
+    -- Surround
+  {
+  "nvim-mini/mini.surround",
+  version = false,
+  config = function()
+      require("mini.surround").setup()
+  end,
+  },
+  -- Syntax highlighting
+  {
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = { "rust", "lua", "toml" },
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
+  },
+
+  -- File finder
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<cr>" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>" },
+    },
+  },
+
+  -- Git gutter
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {},
+  },
+    -- colorscheme
+  {
+  "folke/tokyonight.nvim",
+  priority = 1000,
+  lazy = false,
+  config = function()
+      require("tokyonight").setup({
+          style = "storm",      -- storm, night, moon, day
+          transparent = false,
+          terminal_colors = true,
+      })
+
+      vim.cmd.colorscheme("tokyonight")
+  end,
+  },
+})
